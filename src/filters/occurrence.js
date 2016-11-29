f329b318be913905309db1333941f8ecc8efb9d0
andcastillo <andcastillo@gmail.com>
2016-11-29 10:53:34 -0500

    fix problem with fields that are expected to be numeric but parsing fail

--- src/filters/occurrence.js
+++ src/filters/occurrence.js
@@ -14,21 +14,36 @@ function filterOccurrence(occurrence) {
         //This is an important field and can only be parse if it exists and it is complete
         if(occurrence['location']) {
             if(occurrence['location']['lat'] && occurrence['location']['lon']) {
-                occurrence['location']['lat'] = +occurrence['location']['lat'];
-                occurrence['location']['lon'] = +occurrence['location']['lon'];
+                let lat =  +occurrence['location']['lat'];
+                let lon =  +occurrence['location']['lon'];
+                if(Number.isNaN(lat) || Number.isNaN(lon)) {
+                    delete occurrence['location'];
+                } else {
+                    occurrence['location']['lat'] = lat;
+                    occurrence['location']['lon'] = lon;
+                }
+
             } else {
                 delete occurrence['location'];
             }
 
         }
-        if(occurrence['maximum_elevation'])
+        if(occurrence['maximum_elevation'] && !Number.isNaN(+occurrence['maximum_elevation']))
             occurrence['maximum_elevation'] = +occurrence['maximum_elevation'];
-        if(occurrence['minimum_elevation'])
+        else
+            delete occurrence['maximum_elevation']
+        if(occurrence['minimum_elevation'] && !Number.isNaN(+occurrence['minimum_elevation']))
             occurrence['minimum_elevation'] = +occurrence['minimum_elevation'];
-        if(occurrence['maximum_depth'])
+        else
+            delete occurrence['minimum_elevation'];
+        if(occurrence['maximum_depth'] && !Number.isNaN(+occurrence['maximum_depth']))
             occurrence['maximum_depth'] = +occurrence['maximum_depth'];
-        if(occurrence['minimum_depth'])
+        else
+            delete occurrence['maximum_depth'];
+        if(occurrence['minimum_depth'] && !Number.isNaN(+occurrence['minimum_depth']))
             occurrence['minimum_depth'] = +occurrence['minimum_depth'];
+        else
+            delete occurrence['minimum_depth']
 
         if(occurrence['event_date']){
             try {
