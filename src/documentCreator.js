ca92d78a71d4d326f8f071411fd891998d92d73a
andcastillo <andcastillo@gmail.com>
2016-11-30 11:30:52 -0500

    fix documentCreator
    Check if the document has dwca_id

--- src/documentCreator.js
+++ src/documentCreator.js
@@ -46,81 +46,83 @@ function create(parameters, callback){
 }
 
 function process(dwac, parameters, callback) {
-    let clientElastic = new elasticsearch.Client({
-        host: _config.get('database.elasticSearch.url'),
-        requestTimeout : Infinity
-    });
+    try{
+        let clientElastic = new elasticsearch.Client({
+            host: _config.get('database.elasticSearch.url'),
+            requestTimeout : Infinity
+        });
 
-    if(!dwac['occResource'] || !dwac['resource']) {
-        callback("Error: Could not load the resource file", null);
-    } else {
-        let occResource = filterOccResource(dwac['occResource']);
-        let collection = occResource.collection;
-        delete occResource.collection;
-        let occurrence = dwac['occurrence'];
-        let resource = filterResource(dwac['resource']);
-        let rsID = parameters.resourceID;
-        occResource.id = rsID;
-        resource.id = rsID;
+        if(!dwac['occResource'] || !dwac['resource']) {
+            callback("Error: Could not load the resource file", null);
+        } else {
+            let occResource = filterOccResource(dwac['occResource']);
+            let collection = occResource.collection;
+            delete occResource.collection;
+            let occurrence = dwac['occurrence'];
+            let resource = filterResource(dwac['resource']);
+            let rsID = parameters.resourceID;
+            occResource.id = rsID;
+            resource.id = rsID;
 
-        //Add the missing fields and transform fields
-        let sourcefileid = '';
-        let rgpId = occResource['gbif_package_id'];
-        if(rgpId) {
-            let lastIndexOf = rgpId.lastIndexOf('/');
-            if(lastIndexOf >= 0) {
-                sourcefileid = rgpId.substring(0,lastIndexOf);
+            //Add the missing fields and transform fields
+            let sourcefileid = '';
+            let rgpId = occResource['gbif_package_id'];
+            if(rgpId) {
+                let lastIndexOf = rgpId.lastIndexOf('/');
+                if(lastIndexOf >= 0) {
+                    sourcefileid = rgpId.substring(0,lastIndexOf);
+                }
             }
-        }
-        else{
-            logger.log('warn', 'Could not determine the sourcefileid for ' + parameters.path + ' ...I\'ll invent one');
-            occResource['gbif_package_id'] = randomstring.generate();;
-            resource['gbif_package_id'] = occResource['gbif_package_id'];
-            sourcefileid = occResource['gbif_package_id'];
-        }
+            else{
+                logger.log('warn', 'Could not determine the sourcefileid for ' + parameters.path + ' ...I\'ll invent one');
+                occResource['gbif_package_id'] = randomstring.generate();;
+                resource['gbif_package_id'] = occResource['gbif_package_id'];
+                sourcefileid = occResource['gbif_package_id'];
+            }
 
-        //Save the resource information
-        let promises = [];
-        if(resource) {
-            promises.push(clientElastic.create({
-                index: _config.get('database.elasticSearch.index'),
-                type: 'resource',
-                method: 'post',
-                id: rsID,
-                body: resource
-            }));
-            if(occurrence) {
-                //Save the occurrences
-                for (let index = 0; index < occurrence.length; index++) {
-                    let doc = occurrence[index];
-                    if (doc) {
-                        doc = filterOccurrence(doc);
-                        if(doc) {
-                            doc['sourcefileid'] = sourcefileid;
-                            doc['provider'] = publisher;
-                            doc['resource'] = occResource;
-                            Object.assign(doc.collection, collection);
-                            promises.push(clientElastic.create({
-                                index: _config.get('database.elasticSearch.index'),
-                                type: 'occurrence',
-                                id: rsID + '_' + index,
-                                body: doc
-                            }));/*, function (error, response) {
-                                if(error) {
-                                    console.log(index, error);
-                                }
-                            }));*/
+            //Save the resource information
+            let promises = [];
+            if(resource) {
+                promises.push(clientElastic.create({
+                    index: _config.get('database.elasticSearch.index'),
+                    type: 'resource',
+                    method: 'post',
+                    id: rsID,
+                    body: resource
+                }));
+                if(occurrence) {
+                    //Save the occurrences
+                    for (let index = 0; index < occurrence.length; index++) {
+                        let doc = occurrence[index];
+                        if (doc && doc.dwca_id) {
+                            doc = filterOccurrence(doc);
+                            if(doc) {
+                                doc['sourcefileid'] = sourcefileid;
+                                doc['provider'] = publisher;
+                                doc['resource'] = occResource;
+                                Object.assign(doc.collection, collection);
+                                promises.push(clientElastic.create({
+                                    index: _config.get('database.elasticSearch.index'),
+                                    type: 'occurrence',
+                                    id: rsID + '_' + index,
+                                    body: doc
+                                }));
+                            }
                         }
                     }
                 }
             }
-        }
 
-        Promise.all(promises).then(values =>{
-            callback(null, values);
-        }, reason => {
-            callback(reason, null);
-        });
+            Promise.all(promises).then(values =>{
+                callback(null, values);
+            }, reason => {
+                callback(reason, null);
+            });
+        }
+    }
+    catch(error) {
+        logger.log('error', 'at function process ' + parameters.path, error);
+        callback(error, null);
     }
 }
 
